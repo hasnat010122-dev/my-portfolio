0e4ff18cd4039cf13e6b8e0e83ad93d5b2a1afa7
hasnat010122-dev <hasnat010122@gmail.com>
2026-08-12 14:35:38 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
   build: {
     cssMinify: false,
   },
